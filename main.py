5415fb3daf25585edc0a7843280a76e09b84dd66
Олег <mishin415@yandex.ru>
2023-03-06 23:42:21 +0300

    homework2.1

---
+++ main.py
@@ -0,0 +1,49 @@
+print("Введите пять чисел")
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+if a > b and a > c and a > d and a > e:
+    print("большее число", a)
+if b > a and b > c and b > d and b > e:
+    print("большее число", b)
+if c > a and c > b and c > d and c > e:
+    print("большее число", c)
+if d > a and d > b and d > c and d > e:
+    print("большее число", d)
+if e > a and e > b and e > c and e > d:
+    print("большее число", e)
+if a < b and a < c and a < d and a < e:
+    print("меньшее число", a)
+if b < a and b < c and b < d and b < e:
+    print("меньшее число", b)
+if c < a and c < b and c < d and c < e:
+    print("меньшее число", c)
+if d < a and d < b and d < c and d < e:
+    print("меньшее число", d)
+if e < a and e < b and e < c and e < d:
+    print("меньшее число", e)
+if a % 3 == 0:
+    print("число делится на три", a)
+if b % 3 == 0:
+    print("число делится на три", b)
+if c % 3 == 0:
+    print("число делится на три", c)
+if d % 3 == 0:
+    print("число делится на три", d)
+if e % 3 == 0:
+    print("число делится на три", e)
+if a % 5 != 0:
+    print("число не делится на пять", a)
+if b % 5 != 0:
+    print("число не делится на пять", b)
+if c % 5 != 0:
+    print("число не делится на пять", c)
+if d % 5 != 0:
+    print("число не делится на пять", d)
+if e % 5 != 0:
+    print("число не делится на пять", e)
+
+
+
